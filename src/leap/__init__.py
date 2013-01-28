2d0170b51a064788eb68af75d5f0d4efb40e386f
Isis Lovecruft <isis@torproject.org>
2013-01-28 18:01:00 +0000

    Fixed module structure, again, due to directory layout change.

--- src/leap/__init__.py
+++ src/leap/__init__.py
@@ -1,8 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-import mx
-import util
+from leap import mx
+from leap import util
+from leap.util import version as mxversion
 
 __all__     = ['mx', 'util']
-__author__  = util.version.authors
-__version__ = util.version.getVersion()
+__author__  = mxversion.authors
+__version__ = mxversion.getVersion()
+
+print "Authors: %s" % __author__
+print "Version: %s" % __version__
