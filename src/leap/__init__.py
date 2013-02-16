73d33d4caac1cd5e0be63584f72de92742a2c91a
Isis Lovecruft <isis@torproject.org>
2013-02-16 22:54:53 +0000

    Cleaned up version tracking and printing.

--- src/leap/__init__.py
+++ src/leap/__init__.py
@@ -1,25 +1,12 @@
 # -*- encoding: utf-8 -*-
+"""
+leap/__init__.py
+----------------
+Module intialization file for leap.
+"""
 
-__all__     = ['mx']
-__author__  = version.authors
-__version__ = version.getVersion()
-
-from leap import mx
-from leap.mx import util
 from leap.mx.util import version
 
-
-def _print_authors_nicely():
-    if hasattr(version, 'authors'):
-        assert isinstance(version.authors, list)
-        if len(version.authors) > 0:
-            first = version.authors.pop()
-            __author__ = (" ".join(first[:2]))
-            if len(version.authors) > 0:
-                for auth in version.authors:
-                    joined = " ".join(auth[:2])
-                    __author__ += ("\n%s" % joined)
-                    
-
-print "Version: %s" % version.getVersion()
-print "Authors: %s" % _print_authors_nicely()
+__all__     = ['mx']
+__author__  = version.getAuthors()
+__version__ = version.getVersion()
