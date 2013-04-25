28a5992e9b4c5a9a080ad39ba6483843c99f1c47
Tomas Touceda <chiiph@leap.se>
2013-04-25 12:04:55 -0300

    More cleanup

--- src/leap/__init__.py
+++ src/leap/__init__.py
@@ -1,12 +1,22 @@
 # -*- encoding: utf-8 -*-
+# __init__.py
+# Copyright (C) 2013 LEAP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
-leap/__init__.py
-----------------
 Module intialization file for leap.
 """
 
-from leap.mx.util import version
-
 __all__     = ['mx']
-__author__  = version.getAuthors()
-__version__ = version.getVersion()
