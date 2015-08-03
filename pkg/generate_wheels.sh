6187e0792de6925d22b3956eb3ba8f46646960d3
Parménides GV <parmegv@sdf.org>
2015-08-03 22:46:30 +0200

    [feat] use wheels to install dependencies
    generate_wheels uses $WHEELHOUSE to generate and store the wheels for
    requirements.pip and requirements-testing.pip (if it exists).
    
    pip_install_requirements.sh installs requirements.pip from them if
    possible (if not, then it fetches them from pypi) or, if passed the
    --testing flag, it installs requirements-testing.pip.
    
    - Related: 7327

---
+++ pkg/generate_wheels.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Generate wheels for dependencies
+
+if [ "$WHEELHOUSE" = "" ]; then
+    WHEELHOUSE=$HOME/wheelhouse
+fi
+
+pip wheel --wheel-dir $WHEELHOUSE pip
+pip wheel --wheel-dir $WHEELHOUSE -r pkg/requirements.pip
+if [ -f pkg/requirements-testing.pip ]; then
+    pip wheel --wheel-dir $WHEELHOUSE -r pkg/requirements-testing.pip
+fi
