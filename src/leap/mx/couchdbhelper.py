76df59e41d971a030f3630db96c732efe2577f91
Ruben Pollan <meskio@sindominio.net>
2016-03-11 13:14:34 +0100

    [bug] Check if the account is enabled
    - Resolves: #7961

--- src/leap/mx/couchdbhelper.py
+++ src/leap/mx/couchdbhelper.py
@@ -100,9 +100,10 @@ class ConnectedCouchDB(client.CouchDB):
             pubkey = None
             if result["rows"]:
                 doc = result["rows"][0]["doc"]
-                uuid = doc["user_id"]
-                if "keys" in doc:
-                    pubkey = doc["keys"]["pgp"]
+                if doc["enabled"]:
+                    uuid = doc["user_id"]
+                    if "keys" in doc:
+                        pubkey = doc["keys"]["pgp"]
             return uuid, pubkey
 
         d.addCallback(_get_uuid_and_pubkey_cbk)
