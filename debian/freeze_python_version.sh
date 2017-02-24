6e2adab763d48749c7638c73cc0506250807fe20
varac <varacanero@zeromail.org>
2017-02-24 11:20:24 +0100

    Freeze python version

---
+++ debian/freeze_python_version.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VERSION_FILE="src/leap/mx/_version.py"
+rm ${VERSION_FILE}
+echo y | python setup.py freeze_debianver
+
+# Add the number of commits after last tag to the version string
+add_commits=$(git describe | cut -d'-' -f2)
+sed -i "/ \"version/s/\"$/-${add_commits}\"/" ${VERSION_FILE}
+
+# Remove the -dirty tag
+sed -i 's/-dirty//g' ${VERSION_FILE}
+
+git commit -m "[pkg] freeze debian version" ${VERSION_FILE}
+
+python setup.py version
