489b5614f8c9ca635b9b70330fd966f5a3371fcf
varac <varacanero@zeromail.org>
2017-02-24 12:08:18 +0100

    Use . before git ref in python version

--- debian/freeze_python_version.sh
+++ debian/freeze_python_version.sh
@@ -7,7 +7,7 @@ echo y | python setup.py freeze_debianver
 # Add the number of commits after last tag to the version string
 add_commits=$(git describe | cut -d'-' -f2)
 rev=$(git describe | cut -d'-' -f3)
-sed -i "/ \"version/s/\"$/.${add_commits}+${rev}\"/" ${VERSION_FILE}
+sed -i "/ \"version/s/\"$/.${add_commits}.${rev}\"/" ${VERSION_FILE}
 
 # Remove the -dirty tag
 sed -i 's/-dirty//g' ${VERSION_FILE}
